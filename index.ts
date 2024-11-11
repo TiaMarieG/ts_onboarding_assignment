f8db6555624d216decbe3b7943542d327bb5a751
tmgor <gordon.tia@student.greenriver.edu>
2024-11-11 14:54:35 -0800

    Created fizzbuzz in index.ts and transpiled to index.js

---
+++ index.ts
@@ -0,0 +1,18 @@
+fizzBuzz();
+
+function fizzBuzz() {
+    for (let i: number = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+        else if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
